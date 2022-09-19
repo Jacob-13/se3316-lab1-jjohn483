ca1a01fec82be9282354b2055efd15734770a8e9
Jacob-13 <jjohn483@uwo.ca>
2022-09-19 14:18:29 -0400

    Added pokemon objects
    Added objects and created an array of pokemon objects

--- pokedex-function.js
+++ pokedex-function.js
@@ -1,8 +1,125 @@
-//An array of pokemon
-let pokemonArr = [ 1, 2, 3];
-
 //Pokemon objects
-let pokemon = {
-    name: "Pokemon Name",
-    num: "Pokemon Number"
+let bulbasaur = {
+    name: "bulbasaur",
+    num: 1,
+    description: "Grass/Poison"
 }
+
+let ivysaur = {
+    name: "Ivysaur",
+    num: 2,
+    description: "Grass/Poison"
+}
+
+let venusaur = {
+    name: "Venusaur",
+    num: 3,
+    description: "Grass/Poison"
+}
+
+let charmander = {
+    name: "Charmander",
+    num: 4,
+    description: "Fire"
+}
+
+let charmeleon = {
+    name: "Charmeleon",
+    num: 5,
+    description: "Fire"
+}
+
+let charizard = {
+    name: "Charizard",
+    num: 6,
+    description: "Fire/Flying"
+}
+
+let squirtle = {
+    name: "Squirtle",
+    num: 7,
+    description: "water"
+}
+
+let wartortole = {
+    name: "Wartortle",
+    num: 8,
+    description: "water"
+}
+
+let blastoise = {
+    name: "Blastoise",
+    num: 9,
+    description: "water"
+}
+
+let caterpie = {
+    name: "Caterpie",
+    num: 10,
+    description: "bug"
+}
+
+let metapod = {
+    name: "Metapod",
+    num: 11,
+    description: "bug"
+}
+
+let butterfree = {
+    name: "Butterfree",
+    num: 12,
+    description: "Bug/Flying"
+}
+
+let weedle = {
+    name: "Weedle",
+    num: 13,
+    description: "Bug/Poison"
+}
+
+let kakuna = {
+    name: "Kakuna",
+    num: 14,
+    description: "Bug/Poison"
+}
+
+let beedrill = {
+    name: "Beedrill",
+    num: 15,
+    description: "Bug/Poison"
+}
+
+let pidgey = {
+    name: "Pidgey",
+    num: 16,
+    description: "Normal/Flying"
+}
+
+let pidgeotto = {
+    name: "Pidgeotto",
+    num: 17,
+    description: "Normal/Flying"
+}
+
+let pidgeot = {
+    name: "Pidgeot",
+    num: 18,
+    description: "Normal/Flying"
+}
+
+let rattata = {
+    name: "Rattata",
+    num: 19,
+    description: "Normal"
+}
+
+let raticate = {
+    name: "Raticate",
+    num: 20,
+    description: "Normal"
+}
+
+//An array of pokemon
+let pokemonArr = [ bulbasaur, ivysaur, venusaur, charmander, charmeleon, charizard, 
+    squirtle, wartortole, blastoise, caterpie, metapod, butterfree, weedle, kakuna, 
+    beedrill, pidgey, pidgeotto, pidgeot, rattata, raticate];
