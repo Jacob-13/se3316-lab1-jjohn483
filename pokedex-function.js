a9de3c5927642bad5e70ac170d9e3c743f882e90
Jacob-13 <jjohn483@uwo.ca>
2022-09-20 14:52:00 -0400

    Functioning name search
    Added the functionality of the name search bar called when the search button is pressed

--- pokedex-function.js
+++ pokedex-function.js
@@ -1,5 +1,6 @@
 const numSearch = document.getElementById('numSearch');
 const nameSearch = document.getElementById('nameSearch');
+let result = [];
 
 //Function handles a search from the number search bar
 let numResults = function() {
@@ -8,144 +9,192 @@ let numResults = function() {
         numSearch.value = null;
         alert("Number out of bounds, please try again.");
     } else {
-        alert("Chosen Pokemon...\n\t" + "Name: " + pokemonArr[num - 1].name + "\n\tNumber: "
-        + pokemonArr[num - 1].num + "\n\tDescription: " + pokemonArr[num - 1].description);
-    }
-}
-
-//Funtion handles a search from the name search bar
-let namReults = function() {
-    pokeName = nameSearch.value;
-    if(pokeName.length > 20){
-        alert("Search must only be 20 Characters");
-        nameSearch.value = pokeName.slice(0,pokeName.length - 1);
+        alert("Chosen Pokemon...\n\t" + "Name: " + pokemonArr[0][num - 1].name + "\n\tNumber: "
+        + pokemonArr[0][num - 1].num + "\n\tDescription: " + pokemonArr[0][num - 1].description);
+        numSearch.value = null;
     }
 }
 
 
 //Pokemon objects
-let bulbasaur = {
-    name: "Bulbasaur",
-    num: 1,
-    description: "Grass/Poison"
-}
 
-let ivysaur = {
-    name: "Ivysaur",
-    num: 2,
-    description: "Grass/Poison"
-}
+ let pokemon = {
+    bulbasaur: {
+        name: "Bulbasaur",
+        num: 1,
+        description: "Grass/Poison"
+    },
 
-let venusaur = {
-    name: "Venusaur",
-    num: 3,
-    description: "Grass/Poison"
-}
+    ivysaur: {
+        name: "Ivysaur",
+        num: 2,
+        description: "Grass/Poison"
+    },
 
-let charmander = {
-    name: "Charmander",
-    num: 4,
-    description: "Fire"
-}
+    venusaur: {
+        name: "Venusaur",
+        num: 3,
+        description: "Grass/Poison"
+    },
 
-let charmeleon = {
-    name: "Charmeleon",
-    num: 5,
-    description: "Fire"
-}
+    charmander: {
+        name: "Charmander",
+        num: 4,
+        description: "Fire"
+    },
 
-let charizard = {
-    name: "Charizard",
-    num: 6,
-    description: "Fire/Flying"
-}
+    charmeleon: {
+        name: "Charmeleon",
+        num: 5,
+        description: "Fire"
+    },
 
-let squirtle = {
-    name: "Squirtle",
-    num: 7,
-    description: "Water"
-}
+    charizard: {
+        name: "Charizard",
+        num: 6,
+        description: "Fire/Flying"
+    },
 
-let wartortole = {
-    name: "Wartortle",
-    num: 8,
-    description: "Water"
-}
+    squirtle: {
+        name: "Squirtle",
+        num: 7,
+        description: "Water"
+    },
 
-let blastoise = {
-    name: "Blastoise",
-    num: 9,
-    description: "Water"
-}
+    wartortole: {
+        name: "Wartortle",
+        num: 8,
+        description: "Water"
+    },
 
-let caterpie = {
-    name: "Caterpie",
-    num: 10,
-    description: "Bug"
-}
+    blastoise: {
+        name: "Blastoise",
+        num: 9,
+        description: "Water"
+    },
 
-let metapod = {
-    name: "Metapod",
-    num: 11,
-    description: "Bug"
-}
+    caterpie: {
+        name: "Caterpie",
+        num: 10,
+        description: "Bug"
+    },
 
-let butterfree = {
-    name: "Butterfree",
-    num: 12,
-    description: "Bug/Flying"
-}
+    metapod: {
+        name: "Metapod",
+        num: 11,
+        description: "Bug"
+    },
 
-let weedle = {
-    name: "Weedle",
-    num: 13,
-    description: "Bug/Poison"
-}
+    butterfree: {
+        name: "Butterfree",
+        num: 12,
+        description: "Bug/Flying"
+    },
 
-let kakuna = {
-    name: "Kakuna",
-    num: 14,
-    description: "Bug/Poison"
-}
+    weedle: {
+        name: "Weedle",
+        num: 13,
+        description: "Bug/Poison"
+    },
 
-let beedrill = {
-    name: "Beedrill",
-    num: 15,
-    description: "Bug/Poison"
-}
+    kakuna: {
+        name: "Kakuna",
+        num: 14,
+        description: "Bug/Poison"
+    },
 
-let pidgey = {
-    name: "Pidgey",
-    num: 16,
-    description: "Normal/Flying"
-}
+    beedrill: {
+        name: "Beedrill",
+        num: 15,
+        description: "Bug/Poison"
+    },
 
-let pidgeotto = {
-    name: "Pidgeotto",
-    num: 17,
-    description: "Normal/Flying"
-}
+    pidgey: {
+        name: "Pidgey",
+        num: 16,
+        description: "Normal/Flying"
+    },
 
-let pidgeot = {
-    name: "Pidgeot",
-    num: 18,
-    description: "Normal/Flying"
-}
+    pidgeotto: {
+        name: "Pidgeotto",
+        num: 17,
+        description: "Normal/Flying"
+    },
 
-let rattata = {
-    name: "Rattata",
-    num: 19,
-    description: "Normal"
-}
+    pidgeot: {
+        name: "Pidgeot",
+        num: 18,
+        description: "Normal/Flying"
+    },
 
-let raticate = {
-    name: "Raticate",
-    num: 20,
-    description: "Normal"
+    rattata: {
+        name: "Rattata",
+        num: 19,
+        description: "Normal"
+    },
+
+    raticate: {
+        name: "Raticate",
+        num: 20,
+        description: "Normal"
+    }
 }
 
 //An array of pokemon
-let pokemonArr = [ bulbasaur, ivysaur, venusaur, charmander, charmeleon, charizard, 
-    squirtle, wartortole, blastoise, caterpie, metapod, butterfree, weedle, kakuna, 
-    beedrill, pidgey, pidgeotto, pidgeot, rattata, raticate];
+// let pokemonArr = [ bulbasaur, ivysaur, venusaur, charmander, charmeleon, charizard, 
+//     squirtle, wartortole, blastoise, caterpie, metapod, butterfree, weedle, kakuna, 
+//     beedrill, pidgey, pidgeotto, pidgeot, rattata, raticate];
+
+let pokemonArr = [];
+pokemonArr.push(Object.values(pokemon));
+
+
+console.log(pokemonArr[0]);
+
+
+//Funtion handles a search from the name search bar
+let namReults = function () {
+    pokeName = nameSearch.value;
+
+    if(pokeName.length > 20){
+        alert("Search must only be 20 Characters");
+        nameSearch.value = pokeName.slice(0,pokeName.length - 1);
+    }
+
+    result = pokemonArr[0].filter(pokemon => pokemon.name.includes(pokeName));
+    console.log(result);
+}
+
+//called on btn press, displays the appropriate alert
+let displayResult = function() {
+    resultAmount = result.length;
+    switch(resultAmount) {
+        case 0:
+            alert("No results found");
+            break;
+        case 1: 
+            alert("Chosen Pokemon...\n\t" + "Name: " + result[0].name + "\n\tNumber: "
+            + result[0].num + "\n\tDescription: " + result[0].description);
+            nameSearch.value = null;
+            break;
+        case 2:
+            alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
+    + result[1].name)
+            break;
+        case 3:
+            alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
+            + result[1].name + "\n\t" + result[2].name)
+            break;
+        case 4:
+            alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
+            + result[1].name + "\n\t" + result[2].name + "\n\t"
+            + result[3].name)
+            break;
+        case 5:
+            alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
+            + result[1].name + "\n\t" + result[2].name + "\n\t"
+            + result[3].name + "\n\t" + result [4].name);
+    }
+    nameSearch.value = null;
+}
 
