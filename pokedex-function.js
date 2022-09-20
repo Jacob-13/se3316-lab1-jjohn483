c074a35b0c5c7bf56634cd523becf5d90adbd607
Jacob-13 <jjohn483@uwo.ca>
2022-09-20 17:17:51 -0400

    Cleaned code
    added comments and rearranged code to follow a more logical order. Search of more than 5 pokemon bug fixed.

--- pokedex-function.js
+++ pokedex-function.js
@@ -1,23 +1,8 @@
-const numSearch = document.getElementById('numSearch');
-const nameSearch = document.getElementById('nameSearch');
-let result = [];
-
-//Function handles a search from the number search bar
-let numResults = function() {
-    num = numSearch.value;
-    if (num < 1 || num > 20){
-        numSearch.value = null;
-        alert("Number out of bounds, please try again.");
-    } else {
-        alert("Chosen Pokemon...\n\t" + "Name: " + pokemonArr[0][num - 1].name + "\n\tNumber: "
-        + pokemonArr[0][num - 1].num + "\n\tDescription: " + pokemonArr[0][num - 1].description);
-        numSearch.value = null;
-    }
-}
-
+const numSearch = document.getElementById('numSearch'); //number search bar reference
+const nameSearch = document.getElementById('nameSearch'); //name search bar reference
+let result = []; //an array for storing the filtered results used in the name search function
 
 //Pokemon objects
-
  let pokemon = {
     bulbasaur: {
         name: "Bulbasaur",
@@ -140,34 +125,29 @@ let numResults = function() {
     }
 }
 
-//An array of pokemon
-// let pokemonArr = [ bulbasaur, ivysaur, venusaur, charmander, charmeleon, charizard, 
-//     squirtle, wartortole, blastoise, caterpie, metapod, butterfree, weedle, kakuna, 
-//     beedrill, pidgey, pidgeotto, pidgeot, rattata, raticate];
-
+//An array with an element that is an array of pokemon
 let pokemonArr = [];
 pokemonArr.push(Object.values(pokemon));
-
-
-console.log(pokemonArr[0]);
-
 
 //Funtion handles a search from the name search bar
 let namReults = function () {
     pokeName = nameSearch.value;
 
+    //an alert to keep the number of characters at 20
     if(pokeName.length > 20){
         alert("Search must only be 20 Characters");
         nameSearch.value = pokeName.slice(0,pokeName.length - 1);
     }
 
-    result = pokemonArr[0].filter(pokemon => pokemon.name.includes(pokeName));
+    //stores the filtered search result array in the variable 'result'
+    result = pokemonArr[0].filter(pokemon => pokemon.name.toLowerCase().includes(pokeName.toLowerCase()));
     console.log(result);
 }
 
-//called on btn press, displays the appropriate alert
+//called on btn press, displays the appropriate alert based on number of pokemon that fit the search
 let displayResult = function() {
     resultAmount = result.length;
+
     switch(resultAmount) {
         case 0:
             alert("No results found");
@@ -179,7 +159,7 @@ let displayResult = function() {
             break;
         case 2:
             alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
-    + result[1].name)
+            + result[1].name)
             break;
         case 3:
             alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
@@ -190,11 +170,23 @@ let displayResult = function() {
             + result[1].name + "\n\t" + result[2].name + "\n\t"
             + result[3].name)
             break;
-        case 5:
+        default:
             alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
             + result[1].name + "\n\t" + result[2].name + "\n\t"
             + result[3].name + "\n\t" + result [4].name);
     }
-    nameSearch.value = null;
+    nameSearch.value = null; //clears the search bar
 }
 
+//Function handles a search from the number search bar
+let numResults = function() {
+    num = numSearch.value;
+    if (num < 1 || num > 20){
+        numSearch.value = null;
+        alert("Number out of bounds, please try again.");
+    } else {
+        alert("Chosen Pokemon...\n\t" + "Name: " + pokemonArr[0][num - 1].name + "\n\tNumber: "
+        + pokemonArr[0][num - 1].num + "\n\tDescription: " + pokemonArr[0][num - 1].description);
+        numSearch.value = null; //clears the search bar
+    }
+}
