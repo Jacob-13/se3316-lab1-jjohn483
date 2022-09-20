3f7811fabddaf484e1a24f3a00ec2e72dcfb6f44
Jacob-13 <jjohn483@uwo.ca>
2022-09-20 17:54:38 -0400

    Edit descriptions of pokemon
    Added a more descriptive description and formatted the alert box

--- pokedex-function.js
+++ pokedex-function.js
@@ -7,121 +7,121 @@ let result = []; //an array for storing the filtered results used in the name se
     bulbasaur: {
         name: "Bulbasaur",
         num: 1,
-        description: "Grass/Poison"
+        description: 'Type: Grass/Poison\n\tHeight: 2 04"\n\tWeight: 15.2 lbs\n\tCategory: Seed'
     },
 
     ivysaur: {
         name: "Ivysaur",
         num: 2,
-        description: "Grass/Poison"
+        description: 'Type: Grass/Poison\n\tHeight: 3 03"\n\tWeight: 28.7 lbs\n\tCategory: Seed'
     },
 
     venusaur: {
         name: "Venusaur",
         num: 3,
-        description: "Grass/Poison"
+        description: 'Type: Grass/Poison\n\tHeight: 6 07"\n\tWeight: 220.5 lbs\n\tCategory: Seed'
     },
 
     charmander: {
         name: "Charmander",
         num: 4,
-        description: "Fire"
+        description: 'Type: Fire\n\tHeight: 2 00"\n\tWeight: 18.7 lbs\n\tCategory: Lizard'
     },
 
     charmeleon: {
         name: "Charmeleon",
         num: 5,
-        description: "Fire"
+        description: 'Type: Fire\n\tHeight: 3 07"\n\tWeight: 41.9 lbs\n\tCategory: Flame'
     },
 
     charizard: {
         name: "Charizard",
         num: 6,
-        description: "Fire/Flying"
+        description: 'Type: Fire/Flying\n\tHeight: 5 07"\n\tWeight: 199.5 lbs\n\tCategory: Flame'
     },
 
     squirtle: {
         name: "Squirtle",
         num: 7,
-        description: "Water"
+        description: 'Type: Water\n\tHeight: 1 08"\n\tWeight: 19.8 lbs\n\tCategory: Tiny Turtle'
     },
 
     wartortole: {
         name: "Wartortle",
         num: 8,
-        description: "Water"
+        description: 'Type: Water\n\tHeight: 3 03"\n\tWeight: 49.6 lbs\n\tCategory: Turtle'
     },
 
     blastoise: {
         name: "Blastoise",
         num: 9,
-        description: "Water"
+        description: 'Type: Water\n\tHeight: 5 03"\n\tWeight: 188.5 lbs\n\tCategory: Shellfish'
     },
 
     caterpie: {
         name: "Caterpie",
         num: 10,
-        description: "Bug"
+        description: 'Type: Bug\n\tHeight: 1 00"\n\tWeight: 6.4 lbs\n\tCategory: Worm'
     },
 
     metapod: {
         name: "Metapod",
         num: 11,
-        description: "Bug"
+        description: 'Type: Bug\n\tHeight: 2 04"\n\tWeight: 21.8 lbs\n\tCategory: Cocoon'
     },
 
     butterfree: {
         name: "Butterfree",
         num: 12,
-        description: "Bug/Flying"
+        description: 'Type: Bug/Flying\n\tHeight: 3 07"\n\tWeight: 70.5 lbs\n\tCategory: Butterfly'
     },
 
     weedle: {
         name: "Weedle",
         num: 13,
-        description: "Bug/Poison"
+        description: 'Type: Bug/Poison\n\tHeight: 1 00"\n\tWeight: 7.1 lbs\n\tCategory: Hairy Bug'
     },
 
     kakuna: {
         name: "Kakuna",
         num: 14,
-        description: "Bug/Poison"
+        description: 'Type: Bug/Poison\n\tHeight: 2 00"\n\tWeight: 22.0 lbs\n\tCategory: Cocoon'
     },
 
     beedrill: {
         name: "Beedrill",
         num: 15,
-        description: "Bug/Poison"
+        description: 'Type: Bug/Poison\n\tHeight: 3 03"\n\tWeight: 65.0 lbs\n\tCategory: Poison Bee'
     },
 
     pidgey: {
         name: "Pidgey",
         num: 16,
-        description: "Normal/Flying"
+        description: 'Type: Normal/Flying\n\tHeight: 1 00"\n\tWeight: 4.0 lbs\n\tCategory: Tiny Bird'
     },
 
     pidgeotto: {
         name: "Pidgeotto",
         num: 17,
-        description: "Normal/Flying"
+        description: 'Type: Normal/Flying\n\tHeight: 3 07"\n\tWeight: 66.1 lbs\n\tCategory: Bird'
     },
 
     pidgeot: {
         name: "Pidgeot",
         num: 18,
-        description: "Normal/Flying"
+        description: 'Type: Normal/Flying\n\tHeight: 4 11"\n\tWeight: 87.1 lbs\n\tCategory: Bird'
     },
 
     rattata: {
         name: "Rattata",
         num: 19,
-        description: "Normal"
+        description: 'Type: Normal\n\tHeight: 1 00"\n\tWeight: 7.7 lbs\n\tCategory: Mouse'
     },
 
     raticate: {
         name: "Raticate",
         num: 20,
-        description: "Normal"
+        description: 'Type: Normal\n\tHeight: 2 04"\n\tWeight: 40.8 lbs\n\tCategory: Mouse'
     }
 }
 
@@ -141,7 +141,6 @@ let namReults = function () {
 
     //stores the filtered search result array in the variable 'result'
     result = pokemonArr[0].filter(pokemon => pokemon.name.toLowerCase().includes(pokeName.toLowerCase()));
-    console.log(result);
 }
 
 //called on btn press, displays the appropriate alert based on number of pokemon that fit the search
@@ -153,27 +152,31 @@ let displayResult = function() {
             alert("No results found");
             break;
         case 1: 
-            alert("Chosen Pokemon...\n\t" + "Name: " + result[0].name + "\n\tNumber: "
-            + result[0].num + "\n\tDescription: " + result[0].description);
+            alert(result[0].name + "\n\tNumber: "
+            + result[0].num + "\n\t" + result[0].description);
             nameSearch.value = null;
             break;
         case 2:
-            alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
-            + result[1].name)
+            alert(result[0].name + "\n\tNumber: " + result[0].num + "\n\t" + result[0].description + "\n"
+            + result[1].name + "\n\tNumber: " + result[1].num + "\n\t" + result[1].description)
             break;
         case 3:
-            alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
-            + result[1].name + "\n\t" + result[2].name)
+            alert(result[0].name + "\n\tNumber: " + result[0].num + "\n\t" + result[0].description + "\n"
+            + result[1].name + "\n\tNumber: " + result[1].num + "\n\t" + result[1].description + "\n" 
+            + result[2].name + "\n\tNumber: " + result[2].num + "\n\t" + result[2].description)
             break;
         case 4:
-            alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
-            + result[1].name + "\n\t" + result[2].name + "\n\t"
-            + result[3].name)
+            alert(result[0].name + "\n\tNumber: " + result[0].num + "\n\t" + result[0].description + "\n"
+            + result[1].name + "\n\tNumber: " + result[1].num + "\n\t" + result[1].description + "\n"
+            + result[2].name + "\n\tNumber: " + result[2].num + "\n\t" + result[2].description + "\n"
+            + result[3].name + "\n\tNumber: " + result[3].num + "\n\t" + result[3].description)
             break;
         default:
-            alert("Possible pokemon:\n\t" + result[0].name + "\n\t"
-            + result[1].name + "\n\t" + result[2].name + "\n\t"
-            + result[3].name + "\n\t" + result [4].name);
+            alert(result[0].name + "\n\tNumber: " + result[0].num + "\n\t" + result[0].description + "\n"
+            + result[1].name + "\n\tNumber: " + result[1].num + "\n\t" + result[1].description + "\n"
+            + result[2].name + "\n\tNumber: " + result[2].num + "\n\t" + result[2].description + "\n"
+            + result[3].name + "\n\tNumber: " + result[3].num + "\n\t" + result[3].description + "\n"
+            + result [4].name + "\n\tNumber: " + result[4].num + "\n\t" + result[4].description);
     }
     nameSearch.value = null; //clears the search bar
 }
