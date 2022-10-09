bcdc8693f6e2221fd377e3367756b6b712688154
Jacob-13 <jjohn483@uwo.ca>
2022-10-09 12:08:53 -0400

    Bug Fixes

--- pokedex-function.js
+++ pokedex-function.js
@@ -17,140 +17,140 @@ divE.style.display = "none";
  let pokemonArr = [
     bulbasaur = {
         name: "Bulbasaur",
-        num: 1,
+        num: "01",
         description: 'Type 1: Grass  |  Type 2: Poison  |  Gen: 1  |  Rarity: 4',
         img: "pokemon\\1.png"
     },
 
     ivysaur = {
         name: "Ivysaur",
-        num: 2,
+        num: "02",
         description: 'Type 1: Grass  |  Type 2: Poison  |  Gen: 1  |  Rarity: 5',
         img: "pokemon\\2.png"
     },
 
     venusaur = {
         name: "Venusaur",
-        num: 3,
+        num: "03",
         description: 'Type 1: Grass  |  Type 2: Poison  |  Gen: 1  |  Rarity: 6',
         img: "pokemon\\3.png"
     },
 
     charmander = {
         name: "Charmander",
-        num: 4,
+        num: "04",
         description: 'Type 1: Fire  |  Type 2: N/A  |  Gen: 1  |  Rarity: 4',
         img: "pokemon\\4.png"
     },
 
     charmeleon = {
         name: "Charmeleon",
-        num: 5,
+        num: "05",
         description: 'Type 1: Fire  |  Type 2: N/A  |  Gen: 1  |  Rarity: 5',
         img: "pokemon\\5.png"
     },
 
     charizard = {
         name: "Charizard",
-        num: 6,
+        num: "06",
         description: 'Type 1: Fire  |  Type 2: Flying  |  Gen: 1  |  Rarity: 6',
         img: "pokemon\\6.png"
     },
 
     squirtle = {
         name: "Squirtle",
-        num: 7,
+        num: "07",
         description: 'Type 1: Water  |  Type 2: N/A  |  Gen: 1  |  Rarity: 4',
         img: "pokemon\\7.png"
     },
 
     wartortole = {
         name: "Wartortle",
-        num: 8,
+        num: "08",
         description: 'Type 1: Water  |  Type 2: N/A  |  Gen: 1  |  Rarity: 5',
         img: "pokemon\\8.png"
     },
 
     blastoise = {
         name: "Blastoise",
-        num: 9,
+        num: "09",
         description: 'Type 1: Water  |  Type 2: N/A  |  Gen: 1  |  Rarity: 6',
         img: "pokemon\\9.png"
     },
 
     caterpie = {
         name: "Caterpie",
-        num: 10,
+        num: "10",
         description: 'Type 1: Bug  |  Type 2: N/A  |  Gen: 1  |  Rarity: 1',
         img: "pokemon\\10.png"
     },
 
     metapod = {
         name: "Metapod",
-        num: 11,
+        num: "11",
         description: 'Type 1: Bug  |  Type 2: N/A  |  Gen: 1  |  Rarity: 3',
         img: "pokemon\\11.png"
     },
 
     butterfree = {
         name: "Butterfree",
-        num: 12,
+        num: "12",
         description: 'Type 1: Bug  |  Type 2: Flying  |  Gen: 1  |  Rarity: 4',
         img: "pokemon\\12.png"
     },
 
     weedle = {
         name: "Weedle",
-        num: 13,
+        num: "13",
         description: 'Type 1: Bug  |  Type 2: Poison  |  Gen: 1  |  Rarity: 1',
         img: "pokemon\\13.png"
     },
 
     kakuna = {
         name: "Kakuna",
-        num: 14,
+        num: "14",
         description: 'Type 1: Bug  |  Type 2: Poison  |  Gen: 1  |  Rarity: 3',
         img: "pokemon\\14.png"
     },
 
     beedrill = {
         name: "Beedrill",
-        num: 15,
+        num: "15",
         description: 'Type 1: Bug  |  Type 2: Poison  |  Gen: 1  |  Rarity: 4',
         img: "pokemon\\15.png"
     },
 
     pidgey = {
         name: "Pidgey",
-        num: 16,
+        num: "16",
         description: 'Type 1: Normal  |  Type 2: Flying  |  Gen: 1  |  Rarity: 1',
         img: "pokemon\\16.png"
     },
 
     pidgeotto = {
         name: "Pidgeotto",
-        num: 17,
+        num: "17",
         description: 'Type 1: Normal  |  Type 2: Flying  |  Gen: 1  |  Rarity: 3',
         img: "pokemon\\17.png"
     },
 
     pidgeot = {
         name: "Pidgeot",
-        num: 18,
+        num: "18",
         description: 'Type 1: Normal  |  Type 2: Flying  |  Gen: 1  |  Rarity: 4',
         img: "pokemon\\18.png"
     },
 
     rattata = {
         name: "Rattata",
-        num: 19,
+        num: "19",
         description: 'Type 1: Normal  |  Type 2: N/A  |  Gen: 1  |  Rarity: 1',
         img: "pokemon\\19.png"
     },
 
     raticate = {
         name: "Raticate",
-        num: 20,
+        num: "20",
         description: 'Type 1: Normal  |  Type 2: N/A  |  Gen: 1  |  Rarity: 4',
         img: "pokemon\\20.png"
     }
@@ -179,7 +179,7 @@ let namEntered = function (e) {
 let namInput = function(e) {
     
     //filter results array
-    namVal = e.target.value.toLowerCase();
+    let namVal = e.target.value.toLowerCase();
     result = pokemonArr.filter(pokemon => pokemon.name.toLowerCase().includes(namVal));
 
     //update dynamic listen
@@ -197,40 +197,47 @@ let namInput = function(e) {
 
 // NUMBER SEARCH FUNCTION
 
-//#numSearch onKeyUp
+// Number search key validations.
+// #numSearch onkeydown
 let numEntered = function(e){
-    
-    //filter results
-    result = pokemonArr.filter(character => character.num.toString().includes(e.target.value));
-    updateSearch();
 
-    //prevents a non-number key from being entered
-    if((e.keyCode < 47 || e.keyCode > 58) && e.keyCode != 13){
+    // Validates key entry
+    if((e.keyCode < 47 || e.keyCode > 58) && e.keyCode != 13 && e.keyCode != 8){
         e.preventDefault();
 
-    //enter key functionality
-    } else if (e.keyCode === 13){
+    // Validates char length.
+    } else if ((e.target.value.length >= 2) && e.keyCode != 13 && e.keyCode != 8){
+        e.preventDefault();
 
-        result = pokemonArr.filter(character => character.num.toString().includes(e.target.value));
+    }
+}
 
-        if(result.length > 19){
-            result.length = 0;
-            displayResult();
-        } else {
-            displayResult();
-        }
-
-    // Validates number range
-    } else if (e.target.value < 1 || e.target.value > 20){
+// Result execution.
+// #numSearch onkeyup
+let numInput = function(e) {
+    
+    // Validate number range.
+    if (e.target.value < 0 || e.target.value > 20){
         alert("Number is out of bounds. Please enter a number between 1 and 20")
         numSearch.value = null;
+    }
+    // Filter results.
+    result = pokemonArr.filter(character => character.num.includes(numSearch.value));
+    updateSearch();
+
+    // Display result on enter key.
+    if(e.keyCode == 13 && result.length > 19){
+        result.length = 0;
+        displayResult();
+    } else if (e.keyCode == 13){
+        displayResult();
     }
 }
 
 //onClick for both search buttons
 let searchBtn = function() {
 
-    result = pokemonArr.filter(character => character.num.toString().includes(numSearch.value));
+    result = pokemonArr.filter(character => character.num.includes(numSearch.value));
 
     if(result.length > 19){
         result.length = 0;
@@ -240,7 +247,10 @@ let searchBtn = function() {
     }
 }
 
-//displays the appropriate alert based on search results
+
+// RESULT FUNCTIONS
+
+// Displays alert based on search
 let displayResult = function() {
     resultAmount = result.length;
 
@@ -274,10 +284,10 @@ let displayResult = function() {
             + result[3].name + " | 0" + result[3].num + "\n\t" + result[3].description + "\n"
             + result [4].name + " | 0" + result[4].num + "\n\t" + result[4].description);
     }
-    nameSearch.value = null; //clears the name search bar
-    numSearch.value = null; //clears the num search bar
+    nameSearch.value = null; // Clears the name search bar
+    numSearch.value = null; // Clears the num search bar
 
-    //clears dynamic search list
+    // Clears dynamic search list
     while(listE.firstChild){
         listE.firstChild.remove();
     };
